35fd49963c29d9d1e8ccbea825793ef489009c09
Simoni Washiya <simoni.washiya@fatec.sp.gov.br>
2024-04-06 21:15:54 -0300

    Update TesteProfessor.java

--- TAREFAREPOSICAO1EX02/src/view/TesteProfessor.java
+++ TAREFAREPOSICAO1EX02/src/view/TesteProfessor.java
@@ -1,16 +1,13 @@
 package view;
 
-// Classe de teste para as implementaÁıes de Professor, ProfessorTitular e ProfessorHorista
 public class TesteProfessor {
     public static void main(String[] args) {
-        // Teste com ProfessorTitular
-        ProfessorTitular professorTitular = new ProfessorTitular("Jo„o", "123", 35, 10);
+        ProfessorTitular professorTitular = new ProfessorTitular("Jo√£o", "123", 35, 10);
         double salarioProfessorTitular = professorTitular.calcularSalario();
-        System.out.println("Sal·rio do Professor Titular: R$" + salarioProfessorTitular);
+        System.out.println("Sal√°rio do Professor Titular: R$" + salarioProfessorTitular);
 
-        // Teste com ProfessorHorista
         ProfessorHorista professorHorista = new ProfessorHorista("Maria", "456", 30, 80, 50.0);
         double salarioProfessorHorista = professorHorista.calcularSalario();
-        System.out.println("Sal·rio do Professor Horista: R$" + salarioProfessorHorista);
+        System.out.println("Sal√°rio do Professor Horista: R$" + salarioProfessorHorista);
     }
 }
